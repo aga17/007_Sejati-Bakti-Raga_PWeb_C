72c2e0b6d3f16747c70f04d783242e149019da09
aga17 <87712208+aga17@users.noreply.github.com>
2022-09-21 15:33:28 +0700

    Add files via upload

---
+++ QUIZ 1/main.js
@@ -0,0 +1,14 @@
+const nama = document.getElementById("name");
+const ttl = document.getElementById("ttl");
+const utusan = document.getElementById("utusan");
+const kelurahan = document.getElementById("kelurahan");
+const kecamatan = document.getElementById("kecamatan");
+const telepon = document.getElementById("telepon")
+const form = document.getElementById("form")
+const lomba = document.getElementsByName("lomba")
+
+form.addEventListener("submit",function(){
+    
+    window.alert("berhasil submit")
+   
+})
